6028ae9f12807f5834b9c0b8362c88a2ceb9143e
Ratna Sekhar <ratnasekhar7888@gmail.com>
2026-03-30 09:26:05 -0400

    blah blah main.py

--- backend/main.py
+++ backend/main.py
@@ -55,8 +55,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://studybuddy-gamma-virid.vercel.app/",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
